6e66ac2b93f957bcb97b65c005aed83d08a61129
Shantanu Gupta <50038300+Shantanugupta1118@users.noreply.github.com>
2021-02-25 21:19:17 +0530

    Add files via upload

---
+++ bday.cpp
@@ -0,0 +1,84 @@
+#include<windows.h>
+#include<iostream>
+#include<stdlib.h> 
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+ int i;
+ char h=3,b=4, happy_face=1;
+ system("cls");
+ system("color f5");
+  Sleep(1800);
+ cout<<"\n\n\t\t\tBest Wishes from: Shantanu Gupta\n";
+ Sleep(1000);
+ cout<<"\t\t     To One of my Beautiful Coding Partner:";
+ Sleep(1000);
+ cout<<" \n\t\t   ---------------"<<b<<" Priya Saini "<<b<<"--------------- \n\n";
+ Sleep(500);
+ std::string bDay_wish("\t\tH A P P Y    B I R T H D A Y    B R O . . . . ! !\n");
+ for(i=0; i<bDay_wish.size(); i++) {
+ cout<<bDay_wish[i];
+ Sleep(20);
+ }
+ cout<<"\t";
+ for(i=0;i<70;i++) {
+ cout<<"_";
+ Sleep(20);
+ }
+ 
+ cout<<"\n\n\t\t";
+ system("color E4"); 
+ 
+ std::string s4 ("I don't do it normally, but sometimes in life, \n\t you gotta leave the normal path and do something eccentric(Graph Theory). \n\n\t\tOkay, so here's what I wish for you:\n\n\t May god give you enough challenges so that every day is full of adventure, \n\t and also enough strength to face those challenges so that every day \n\t is full of joy and ends with a great sense of satisfaction. \n\t May each day, be a great one for you, and you never feel sad, dejected, alone, or in doubt.\n\t I wish your cache(Computer Architecture) be always filled with happiness, \n\t your memory be filled with great moments, your HDD be loaded with  tons \n\t of great things to do to find your inner peace and satiate your \n\t personal journey and your CPU always craving to learn more and become better.\n\n\t    cutting short, \n\n\tWhat advice may I give to someone who is already smart+healthy+brainy+hardworking ?\n\tRun even faster. Be more focused. \n\tBut most importantly enjoy the transition. \n\tBe delighted to foresee the person you are becoming. \n\tLove what you do. Don't do it just because you have to do it. \n\n\t\t\t  Really Love it. \n\n\tWhat is the most important thing that I can tell you on this day ? \n\t\t\t You rock Period.\n\t\t   You asked for it, so you get it.. \n\t    Above text is inherently ambiguous(Theory of Automata), \n\tit will be really tough to parse, forget getting a semantic translation. ;)\n\n\t\t    JUST READ & FORGET...........!!");
+ cout<<"\t";
+ for(i =0; i<s4.size(); i++) {
+ 	cout<<s4[i];
+ 	Sleep(30);
+ } 
+ cout<<"\n\n";
+ Sleep(1200);
+ cout<<"\t\t\t\ti i i i i\n";
+ 
+ Sleep(1200);
+ cout<<"\t\t\t\ti i i i i\n";
+ 
+ Sleep(1200);
+ cout<<"\t\t\t      __i_i_i_i_i__\n";
+ 
+ Sleep(1200);
+ cout<<"\t\t\t     |"<<h<<"           "<<h<<"|\n";
+ 
+ Sleep(1200);
+ cout<<"\t\t\t   __| "<<h<<"  26/02  "<<h<<" |__\n";
+ 
+ Sleep(1200);
+ cout<<"\t\t\t  | "<<h<<"   "<<h<<"       "<<h<<"   "<<h<<" |\n";
+
+ Sleep(1200);
+ cout<<"\t\t\t__|"<<h<<"   HAPPY     "<<h<<"   "<<h<<"|__\n";
+
+ Sleep(1200);
+ cout<<"\t\t       | "<<h<<"   "<<h<<"     BIRTHDAY    "<<h<<" |\n";
+
+ Sleep(1200);
+ cout<<"\t\t       |"<<h<<"  "<<h<<"     "<<h<<"    "<<h<<"     "<<h<<"   "<<h<<"|\n"; 
+ 
+ 
+ Sleep(1200);
+ cout<<"\t\t       | "<<h<<" "<<h<<" "<<h<<" "<<h<<" P R I Y A "<<h<<" "<<h<<" "<<h<<" |\n";
+    
+ Sleep(1200);
+ cout<<"\t\t       |_________________________|\n";    
+    
+ cout<<"\n\n\n\n\n"<<happy_face<<" "; 
+ for(i=0; i<=100;i++)
+ 	cout<<"_";
+cout<<" "<<happy_face<<"\n\n\n\n\n\n\n\n";
+//cout<<"\t\t Best Wishes for ur"
+ system("pause");
+ return 0;
+}
